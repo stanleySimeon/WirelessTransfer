e8ac12fcfeed960cd4faa1f9489aa458800eb4a0
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-26 20:52:06 -0500

    Update script

--- server.js
+++ server.js
@@ -9,82 +9,97 @@ const mdns = require('mdns');
 const browser = mdns.createBrowser(mdns.tcp('http'));
 
 browser.on('serviceUp', service => {
-    console.log('Service up:', service.name, service.addresses);
+  console.log('Service up:', service.name, service.addresses);
 });
 
 browser.on('serviceDown', service => {
-    console.log('Service down:', service.name, service.addresses);
+  console.log('Service down:', service.name, service.addresses);
 });
 
 browser.start();
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
 // Serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  req.socket.setNoDelay(true);
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Serve static files from the "/public" directory
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen for socket connections
 io.on('connection', socket => {
-    console.log('Client connected:', socket.id);
+  console.log('A user connected:', socket.id);
 
-    // Send a list of available peers to all clients
+  // Send a list of available peers to all clients
+  const peers = Object.keys(io.sockets.sockets)
+    .filter(id => id !== socket.id);
+  io.emit('peers', peers);
+
+  // Handle incoming connections
+  socket.on('peer', peer => {
+    console.log('Peer connection:', socket.id, peer);
+
+    // Send a list of available peers to the new peer
     const peers = Object.keys(io.sockets.sockets)
-        .filter(id => id !== socket.id);
-    io.emit('peers', peers);
+      .filter(id => id !== socket.id && id !== peer);
+    io.to(peer).emit('peers', peers);
+  });
 
-    // Handle incoming file transfers
-    socket.on('send file', (data, peer) => {
-        // Find the socket for the specified peer
-        const peerSocket = io.sockets.sockets[peer];
+  // Handle incoming file transfers
+  socket.on('send file', (data, peer) => {
+    const peerSocket = io.sockets.sockets[peer];
 
-        // Check if the peer socket exists
-        if (!peerSocket) {
-            console.error(`Peer ${peer} not found`);
-            return;
+    // Check if the peer socket exists
+    if (!peerSocket) {
+      console.error(`Peer ${peer} not found`);
+      return;
+    }
+
+    // Write the file to disk
+    const filename = path.join(__dirname, 'uploads', data.filename);
+    fs.writeFile(filename, data.data, err => {
+      if (err) {
+        console.error(`Error writing file ${filename}: ${err}`);
+        return;
+      }
+
+      // Send a confirmation message to the sender
+      socket.emit('file sent', {
+        filename: data.filename,
+        size: data.data.byteLength
+      });
+
+      // Send the file to the peer
+      fs.readFile(filename, (err, fileData) => {
+        if (err) {
+          console.error(`Error reading file ${filename}: ${err}`);
+          return;
         }
 
-        // Write the file to disk
-        const filename = path.join(__dirname, 'uploads', data.filename);
-        fs.writeFile(filename, Buffer.from(data.data), err => {
-            if (err) {
-                console.error(`Error writing file ${filename}: ${err}`);
-                return;
-            }
-            console.log(`File ${filename} saved`);
-
-            // Send a confirmation message to the sender
-            socket.emit('file sent', {
-                filename: data.filename,
-                size: data.data.byteLength
-            });
-
-            // Send the file to the peer
-            fs.readFile(filename, (err, fileData) => {
-                if (err) {
-                    console.error(`Error reading file ${filename}: ${err}`);
-                    return;
-                }
-                peerSocket.emit('file received', {
-                    filename: data.filename,
-                    data: fileData.buffer
-                });
-            });
+        peerSocket.emit('file received', {
+          filename: data.filename,
+          data: fileData
         });
+      });
     });
+  });
 
-    // Handle socket disconnections
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+
 });
 
 // Start the server
 server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  browser.stop();
+  process.exit();
 });
